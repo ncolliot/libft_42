3ae98e7d1d307b322b4e7a02556f54fca88b9171
Nino C.Viñals <ninoc.vinals@MacBook-Pro-de-Nino.local>
2022-09-28 11:10:21 +0200

    substr ok created strtrim.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ncolliot <ncolliot@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 14:16:17 by ncolliot          #+#    #+#             */
-/*   Updated: 2022/09/27 16:32:25 by ncolliot         ###   ########.fr       */
+/*   Updated: 2022/09/27 21:18:07 by ncolliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
     
 	i = start;
 	j = 0;
-	if(!s || !(str = (char*)malloc(len + 1)));
+	if(!s || !(str = (char*)malloc(len + 1)))
 		return(0);
 	while(j < len && i < ft_strlen(s))
 	{
