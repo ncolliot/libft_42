827d704f23735bc441cfda72cd3575297e2353b3
Nino C.Viñals <ninoc.vinals@MacBook-Pro-de-Nino.local>
2022-11-30 15:05:16 +0100

    strtrim ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ncolliot <ncolliot@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 12:25:35 by ncolliot          #+#    #+#             */
-/*   Updated: 2022/11/21 11:27:49 by ncolliot         ###   ########.fr       */
+/*   Updated: 2022/11/30 00:01:42 by ncolliot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,25 @@
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	j;
-	char	*str;
 
-	i = 0;
-	if (!s1)
+	if (!s1 || !set)
 		return (0);
-	j = ft_strlen(s1);
-	str = NULL;
-	if (s1 != NULL && set != NULL)
-	{
-		while (s1[i] && ft_strchr(set, s1[i]))
-			i++;
-		while (s1[j - 1] && ft_strchr(set, s1[j - 1]) && j > i)
-			j--;
-		str = (char *)malloc(sizeof(char) * (j - i + 1));
-		if (str)
-			ft_strlcpy(str, &s1[i], j - i + 1);
-	}
-	return (str);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	i = ft_strlen(s1);
+	while (i && ft_strchr(set, s1[i]))
+		i--;
+	return (ft_substr(s1, 0, i + 1));
+}
+
+#include <stdio.h>
+
+int		main()
+{
+	char *str = "NinoColliot";
+	char *out = "Nin";
+	char *j;
+	j = ft_strtrim(str, out);
+	printf("%s\n", str);
+	printf("%s\n", j);
 }
